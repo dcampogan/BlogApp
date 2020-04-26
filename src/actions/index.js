0609316ddc1e03abb903065de64daa33b6a95001
Dwayne Campogan <dcampogan@gmail.com>
2020-04-25 18:34:45 -0700

    Displaying the User Header

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,12 @@ export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
   };
 };
 
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data});
+};
