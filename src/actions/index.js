3532d7c663f37da705fd8f7136dd7bbdb992e5ec
Dwayne Campogan <dcampogan@gmail.com>
2020-04-27 20:51:53 -0700

    Finding Unique User Ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,32 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
 
-export const fetchPosts = () => {
-  return async (dispatch) => {
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response.data })
-  };
+    dispatch({ type: 'FETCH_POSTS', payload: response.data }) ;
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) =>  async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data});
+  dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+// MEMOIZED (LODASH) SOLUTION TO HANDLING "OVERFETCHING PROBLEM"
+// export const fetchUser = (id) =>  (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data});
+// });
